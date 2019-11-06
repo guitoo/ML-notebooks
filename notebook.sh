b242ab16765e41dfe9ab7e8b0bf3940f1d8f449d
Guillaume <g.meurisse.ia@gmail.com>
2019-11-06 19:51:48 +0100

    Add start and stop argument to notebook.sh script

--- notebook.sh
+++ notebook.sh
@@ -1,3 +1,29 @@
-set -a
-source ./env
-jupyter notebook
+#!/bin/bash
+
+PIDFile=./.notebook.pid
+
+if [[ $1 == 'start' ]] ; then
+    if [[ -f "$PIDFile" ]]; then
+	echo 'notebook already started'
+    else
+	echo 'starting notebook'
+	set -a
+	source ./.env
+
+	jupyter notebook </dev/null &>/dev/null &
+	echo $! > $PIDFile
+    fi
+elif [[ $1 == 'stop' ]] ; then
+    if [[ -f "$PIDFile" ]]; then
+	echo 'stopping notebook'
+	kill $(cat $PIDFile)
+	rm $PIDFile
+    else
+	echo 'notebook not started'
+    fi
+else
+    echo 'start the notebook with'
+    echo './notebook.sh start'
+    echo 'stop the notebook with'
+    echo './notebook.sh stop'
+fi
