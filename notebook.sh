9a1cb4c88bf98fa320e1919fef07cbcee59b477c
Guillaume <g.meurisse.ia@gmail.com>
2019-11-12 00:24:38 +0100

    Replace if with case

--- notebook.sh
+++ notebook.sh
@@ -2,28 +2,31 @@
 
 PIDFile=./.notebook.pid
 
-if [[ $1 == 'start' ]] ; then
-    if [[ -f "$PIDFile" ]]; then
-	echo 'notebook already started'
-    else
-	echo 'starting notebook'
-	set -a
-	source ./.env
-
-	jupyter notebook </dev/null &>/dev/null &
-	echo $! > $PIDFile
-    fi
-elif [[ $1 == 'stop' ]] ; then
-    if [[ -f "$PIDFile" ]]; then
-	echo 'stopping notebook'
-	kill $(cat $PIDFile)
-	rm $PIDFile
-    else
-	echo 'notebook not started'
-    fi
-else
-    echo 'start the notebook with'
-    echo './notebook.sh start'
-    echo 'stop the notebook with'
-    echo './notebook.sh stop'
-fi
+case "$1" in
+    start)
+	if [[ -f "$PIDFile" ]]; then
+	    echo 'notebook already started'
+	else
+	    echo 'starting notebook'
+	    set -a
+	    source ./.env
+	    
+	    jupyter notebook </dev/null &>/dev/null &
+	    echo $! > $PIDFile
+	fi
+	;;
+    stop)
+	if [[ -f "$PIDFile" ]]; then
+	    echo 'stopping notebook'
+	    kill $(cat $PIDFile)
+	    rm $PIDFile
+	else
+	    echo 'notebook not started'
+	fi
+	;;
+    *)
+	echo 'start the notebook with'
+	echo './notebook.sh start'
+	echo 'stop the notebook with'
+	echo './notebook.sh stop'
+esac
