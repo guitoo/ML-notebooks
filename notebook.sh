1b60be21f2b64c6caf3f49ec9530b1816a4dac74
Guillaume <g.meurisse.ia@gmail.com>
2019-11-16 13:39:28 +0100

    Add restart function in notebook launcher script

--- notebook.sh
+++ notebook.sh
@@ -2,31 +2,55 @@
 
 PIDFile=./.notebook.pid
 
+
+start (){
+    if [[ -f "$PIDFile" ]]; then
+	echo 'notebook already started'
+    else
+	echo 'starting notebook'
+	set -a
+	source ./.env
+	
+	jupyter notebook </dev/null &>/dev/null &
+	echo $! > $PIDFile
+    fi
+}
+
+stop (){
+    if [[ -f "$PIDFile" ]]; then
+	echo 'stopping notebook'
+	kill $(cat $PIDFile)
+	rm $PIDFile
+    else
+	echo 'notebook not started'
+    fi
+}
+
+restart (){
+    stop
+    start
+}
+
+reset (){
+    rm $PIDFile
+}
+
+usage (){
+  echo './notebook.sh {start|stop|restart|reset}'
+}
+
+
 case "$1" in
     start)
-	if [[ -f "$PIDFile" ]]; then
-	    echo 'notebook already started'
-	else
-	    echo 'starting notebook'
-	    set -a
-	    source ./.env
-	    
-	    jupyter notebook </dev/null &>/dev/null &
-	    echo $! > $PIDFile
-	fi
+	start
 	;;
     stop)
-	if [[ -f "$PIDFile" ]]; then
-	    echo 'stopping notebook'
-	    kill $(cat $PIDFile)
-	    rm $PIDFile
-	else
-	    echo 'notebook not started'
-	fi
+	stop
+	;;
+    restart)
+	restart
 	;;
     *)
-	echo 'start the notebook with'
-	echo './notebook.sh start'
-	echo 'stop the notebook with'
-	echo './notebook.sh stop'
+	usage
+	;;
 esac
